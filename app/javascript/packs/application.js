5d6970f4f9e87d46cf34c03705a3b843f595ffed
Carlos Alve <carlosalves369m@hotmail.com>
2022-06-21 18:42:56 -0300

    :heavy_plus_sign: add `bootstrap` dependency

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,11 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import bootstrap from "bootstrap";  
+import BootstrapStorage from "bootstrap/modal";  
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+bootstrap.start()
+BootstrapStorage.start()
